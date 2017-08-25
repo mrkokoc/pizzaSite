78942ce784b5a7dc7aa5a05bb020446abca90fcb
mrkokoc <ilia.guskov@gmail.com>
2017-08-26 01:49:04 +0300

    bootstrap added

--- site/pizzaSite.js
+++ site/pizzaSite.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var weatherData = require('./lib/weatherData.js');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(function (req, res, next) {
 app.get('/', function (req, res) {
     res.render('home');
 });
+
 app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
@@ -82,6 +84,38 @@ app.get('/data/nursery-rhyme', function (req, res) {
         adjective: 'bushy',
         noun: 'heck'
     });
+});
+
+app.get('/newsletter', function (req, res) {
+    res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(),
+        month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) return res.redirect(303, '/error');
+        console.log('received fields: ');
+        console.log(fields);
+        console.log('received files: ');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+
+app.post('/process', function (req, res) {
+    if (req.xhr || req.accepts('json.html') === 'json') {
+        res.send({success: true});
+    } else {
+        res.redirect(303, '/thank-you');
+    }
 });
 
 // headers test
