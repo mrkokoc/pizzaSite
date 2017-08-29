d3596f260a337008766a6d73b4488e6687047f42
mrkokoc <ilia.guskov@gmail.com>
2017-08-29 15:15:26 +0300

    added flash message

--- site/pizzaSite.js
+++ site/pizzaSite.js
@@ -2,6 +2,10 @@ var express = require('express');
 var fortune = require('./lib/fortune.js');
 var weatherData = require('./lib/weatherData.js');
 var formidable = require('formidable');
+var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials');
+var session = require('express-session');
+
 
 var app = express();
 
@@ -25,6 +29,32 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(require('body-parser').urlencoded({extended: true}));
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
+app.use(function (req, res, next) {
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
+
+app.use('/upload', function (req, res, next) {
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function () {
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function () {
+            return '/uploads/' + now;
+        }
+    })(req, res, next);
+});
+
 // set 'showTests' context property if the querystring contains test=1
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
@@ -39,6 +69,7 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
     res.render('home');
+    res.clearCookie('signed_testCookie');
 });
 
 app.get('/about', function (req, res) {
@@ -88,6 +119,52 @@ app.get('/data/nursery-rhyme', function (req, res) {
 
 app.get('/newsletter', function (req, res) {
     res.render('newsletter', {csrf: 'CSRF token goes here'});
+    res.cookie('signed_testCookie', 'nom nom', {signed: true, maxAge: 2000, httpOnly: true});
+});
+
+function NewsletterSignup() {
+}
+
+NewsletterSignup.prototype.save = function (cb) {
+    cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function (req, res) {
+    var name = req.body.name || '', email = req.body.email || '';
+    // input validation
+    if (!email.match(VALID_EMAIL_REGEX)) {
+        if (req.xhr) return res.json({error: 'Invalid name email address.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was  not valid.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({name: name, email: email}).save(function (err) {
+        if (err) {
+            if (req.xhr) return res.json({error: 'Database error.'});
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.'
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if (req.xhr) return res.json({success: true});
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+});
+
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
 });
 
 app.get('/contest/vacation-photo', function (req, res) {
